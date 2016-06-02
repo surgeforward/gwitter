51411dc3daaf6414e3d3dc8a3c01015eff401861
Theresa Judkins <tjudkins@surgeforward.com>
2016-06-01 22:06:37 -0700

    webservice skeleton initial commit.

--- webservice/serve.go
+++ webservice/serve.go
@@ -1,9 +1,25 @@
 package webservice
 
 import (
-  "fmt"
-)
+        "fmt"
+        "log"
+            "net/http"
+        )
+
+func handleSend(w http.ResponseWriter, r *http.Request) {
+    fmt.Println("Handling Send Request")
+}
+
+func handleList(w http.ResponseWriter, r *http.Request) {
+    fmt.Println("Handling List Request")
+}
 
 func Serve() {
-  fmt.Println("serve webservice ... tbd")
+    http.HandleFunc("/send", handleSend)
+    http.HandleFunc("/list", handleList)
+    err := http.ListenAndServe(":9090", nil) // set listen port
+    if err != nil {
+        log.Fatal("ListenAndServe: ", err)
+    }
 }
+
